e72e9da605a1fab0ba16292598f37624d679d5fe
Hp <Hp@LAPTOP-LF3DIR8S>
2022-07-05 00:03:36 +0700

    Menambah Halaman Login

---
+++ src/vanof/LoginController.java
@@ -0,0 +1,33 @@
+
+package vanof;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+
+public class LoginController implements Initializable {
+    
+    OpenScene bukaScene = new OpenScene();
+    
+    @FXML
+    private BorderPane mainPane;
+    
+    @FXML
+    private void loginButtonAction(ActionEvent event) {
+        Pane halaman = bukaScene.getPane("Masuk1");
+        mainPane.setLeft(halaman);
+        System.out.println("Button Masuk Is Clicked!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/vanof/Masuk1Controller.java
@@ -0,0 +1,23 @@
+
+package vanof;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Hp
+ */
+public class Masuk1Controller implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
