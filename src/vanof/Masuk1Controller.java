92d62dc72c302246e0f88dc6db34e1efdb6db8c3
Hp <Hp@LAPTOP-LF3DIR8S>
2022-07-05 22:18:34 +0700

    Menambah Halaman Login

--- src/vanof/Masuk1Controller.java
+++ src/vanof/Masuk1Controller.java
@@ -5,16 +5,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Hp
- */
+
 public class Masuk1Controller implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- src/vanof/loginDocumentController.java
+++ src/vanof/loginDocumentController.java
@@ -11,7 +11,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.layout.BorderPane;
 
-public class FXMLDocumentController_1 implements Initializable {
+public class loginDocumentController implements Initializable {
     
     OpenScene bukaScene = new OpenScene();
     
